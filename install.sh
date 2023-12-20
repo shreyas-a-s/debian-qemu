fc5f7d956c2ae7a78e7803cd67f4f373cb18ec79
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-20 13:53:58 +0530

    Lots of updates

--- install.sh
+++ install.sh
@@ -8,50 +8,61 @@ fi
 
 # Checking if virualisation is enabled or not
 if [ "$(grep -Ec '(vmx|svm)' /proc/cpuinfo)" -eq '0' ]; then
-    echo "Virtualization is not enabled. If your Processor supports virtualisation, go to bios settings and enable VT-x(Virtualization Technology Extension) for Intel processor and AMD-V for AMD processor." 2>&1
-    exit 1
+  echo "Virtualization is not enabled. If your Processor supports virtualisation, go to bios settings and enable VT-x(Virtualization Technology Extension) for Intel processor and AMD-V for AMD processor." 2>&1
+  exit 1
 fi
 
 # Actual installation
 if command -v apt-get > /dev/null; then
+  # Install necessary programs
+  sudo apt-get -y install qemu-kvm qemu-system qemu-utils python3 python3-pip libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon
 
-  sudo apt-get -y install qemu-kvm qemu-system qemu-utils python3 python3-pip libvirt-clients libvirt-daemon-system bridge-utils virtinst libvirt-daemon virt-manager
-	
-	# Add user to libvirt to Allow access to VMs
-	sudo usermod -aG libvirt "$USER"
-	sudo usermod -aG libvirt-qemu "$USER"
-	sudo usermod -aG kvm "$USER"
-	sudo usermod -aG input "$USER"
-	sudo usermod -aG disk "$USER"
-
+  # Add user to libvirt group to Allow access to VMs
+  sudo usermod -aG libvirt "$USER"
+  sudo usermod -aG libvirt-qemu "$USER"
+  sudo usermod -aG kvm "$USER"
+  sudo usermod -aG input "$USER"
+  sudo usermod -aG disk "$USER"
 elif command -v pacman > /dev/null; then
+  # Install necessary programs
+  sudo pacman -S --noconfirm qemu-base virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat ebtables iptables libguestfs
 
-	sudo pacman -S qemu-base virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat ebtables iptables libguestfs --noconfirm
-	
-	if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
-	  sudo pacman -S gnome-boxes --noconfirm
-	else
-	  sudo pacman -S virt-manager --noconfirm
-	fi
-	  
-	sudo usermod -aG libvirt $(whoami)
-	newgrp libvirt
-	
-	sudo systemctl enable --now libvirtd
+  # Add user to libvirt group to Allow access to VMs
+  sudo usermod -aG libvirt $(whoami)
+  newgrp libvirt
+fi
 
+if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
+  if command -v apt-get > /dev/null; then
+    sudo apt-get install -y gnome-boxes
+  elif command -v pacman > /dev/null; then
+    sudo pacman -S --noconfirm gnome-boxes
+  fi
+else
+  if command -v apt-get > /dev/null; then
+    sudo apt-get install -y virt-manager
+  elif command -v pacman > /dev/null; then
+    sudo pacman -S --noconfirm virt-manager
+  fi
+fi
+
+# Enable libvirtd service using systemd if available
+if command -v systemctl > /dev/null; then
+  sudo systemctl enable --now libvirtd
 fi
 
 # Checking if libvirtd.service was enabled correctly
-if [ "$(systemctl status libvirtd.service | awk 'NR==2{print $4}')" != "enabled;" ]; then 
-    echo "libvirtd.service is not enabled. Please check why."
+if [ "$(systemctl status libvirtd.service | awk 'NR==2{print $4}')" != "enabled;" ]; then
+  echo "libvirtd.service is not enabled. Please check why."
 fi
 
 # Start default network for networking
 sudo virsh net-start default
 sudo virsh net-autostart default
 if [ "$(sudo virsh net-list --all | awk 'NR==3{print $3}')" != "yes" ]; then 
-    echo "Default network for virtual machines is not set to autostart. Please check why."
+  echo "Default network for virtual machines is not set to autostart. Please check why."
 fi
 
 # The End
 echo "Reboot the system."
+
